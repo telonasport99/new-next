588852a0ab131f823c71c663f8b078d98f6ab1e4
stacy <ada.davis27@gmail.com>
2023-06-23 17:31:26 +0300

    prepared lesson

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import type {AppProps} from 'next/app';
 import {ReactElement, ReactNode} from 'react';
 import {NextPage} from 'next';
-import {useLoader} from 'assets/hooks/useLoader';
-import 'styles/nprogress.css'
 
 export type NextPageWithLayout<P = {}> = NextPage<P> & {
     getLayout?: (page: ReactElement) => ReactNode;
@@ -13,8 +11,6 @@ type AppPropsWithLayout = AppProps & {
 };
 
 export default function App({Component, pageProps}: AppPropsWithLayout) {
-    useLoader()
-
     const getLayout = Component.getLayout ?? ((page) => page);
 
     return getLayout(<Component {...pageProps} />)
